78b4fa554a85fad818430da97a74fd1c82610c70
rkundala <rkundala@gmail.com>
2023-04-06 19:00:01 -0400

    aa

--- features/SignIn.feature
+++ features/SignIn.feature
@@ -3,15 +3,9 @@ Feature: SignIn
   I want to log in to the website 
   So I can permform business transactions
 
-  Scenario #1: Successful Sign in to the website
+  Scenario: Successful Sign in to the website
     Given A user brings up the signin page
     When A user clicks Sign-in
-    And A user enters a valid Username <username> and Pssword <password>
-    And A user clicks Login
+    And A user enters a valid Username   "Ram.com" and Pssword  "pwd" 
+    And A user clicks SingIn
     Then A user should be successfully logged into the site
-
-  Scenario #2: Unsuccessful Log in to the website
-    Given A user brings up the signin pop-up
-    When  A user enters a valid Username <username> and Pssword <password>
-    And A user clicks Login
-    Then A user should not be successfully logged into the site
--- features/steps/BatchlistSteps.js
+++ features/steps/BatchlistSteps.js
@@ -1,30 +1,44 @@
-const { Given, When, Then }  = require('@cucumber/cucumber');
+const { Given, When, Then } = require('@cucumber/cucumber');
 
 //    Given Logged into the system
 //        Undefined. Implement with the following snippet:
 
-         Given('Logged into the system', function () {
-           // Write code here that turns the phrase above into concrete actions
-           console.log("Logged into the system");
-           return 'pending';
-         });
+Given('Logged into the system', function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("Logged into the system");
+
+  Category cat = new Category();
+
+  
+  return 'pending';
+});
 
 //    When we request the batch list
 //        Undefined. Implement with the following snippet:
 
-         When('we request the batch list', function () {
-           // Write code here that turns the phrase above into concrete actions
-           console.log("we request the batch list");
+When('we request the batch list', function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("we request the batch list");
 
-           return 'pending';
-         });
+  return 'pending';
+});
 
 //    Then we receive the list of batch
 //        Undefined. Implement with the following snippet:
 
-         Then('we receive the list of batch', function () {
-           // Write code here that turns the phrase above into concrete actions
-           console.log("we receive the list of batch");
+Then('we receive the list of batch', function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("we receive the list of batch");
 
-           return 'pending';
-         });
+  return 'pending';
+});
+
+class Category {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.print = function () {
+      console.log(`${this.name} - ${this.id}`);
+    };
+  }
+}
