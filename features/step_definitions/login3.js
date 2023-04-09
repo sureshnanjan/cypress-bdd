b20bc067492f069c4abdd6c7c68fa8e13748fc8e
rkundala <rkundala@gmail.com>
2023-04-08 22:22:01 -0400

    3

--- features/step_definitions/login3.js
+++ features/step_definitions/login3.js
@@ -2,16 +2,26 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
 
 
-
+// login
 Given("Application login page displayed", function () {
-   this.credentials={username:"",password:""};
+    this.credentials = { username: 0, password: "password" };
 });
 
 When("user credentials enterd", function () {
-    this.credentials={username:10,password:"pwd"};
+
+    let uname = 10;
+    let pwd = "pwd";
+    if (this.credentials['username'] !== uname || !(this.credentials['password'] === pwd)) {
+        this.credentials['username'] = uname;
+        this.credentials['password'] = pwd;
+        console.log("change");
+    } else {
+        console.log("no change");
+    }
 
 });
 
 Then("user successfully login into application", function () {
-    assert.ok(this.credentials['username'] ===10 && this.credentials['password'] === "pwd", 'Expected us to be prepared to start and also started.');
+
+    assert.ok(this.credentials['username'] === 10 && this.credentials['password'] === "pwd", 'Expected us to be prepared to start and also started.');
 });
